eddd46a42536293001301f24b548ce5e97e42171
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2021-05-22 00:22:38 -0500

    Style done and color selector working

--- canvas.js
+++ canvas.js
@@ -1,5 +1,18 @@
 let container = document.getElementById("container")
 let colorSelector = document.getElementById("color")
+let colorShow = document.getElementById("color-show")
+let colorSelected = document.getElementById("color-selected")
+
+colorSelector.addEventListener("input", function() {
+    colorShow.style.backgroundColor = colorSelector.value
+    colorSelected.value = colorSelector.value
+})
+
+colorSelected.addEventListener("input", function(e) {
+    colorSelector.value = e.target.value.toString()
+    colorShow.style.backgroundColor = e.target.value.toString()
+    console.log(e.target.value)
+})
 
 const createGrid = (function() {
     for(let y = 0;y < 16;y++) {
